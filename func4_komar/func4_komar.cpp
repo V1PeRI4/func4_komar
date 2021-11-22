880b570a4401496b7fc14749777bd5fa1e156368
V1PeRI4 <V1PeRI4@DESKTOP-C59PJAE>
2021-11-22 21:52:01 +0300

    Добавьте файлы проекта.

---
+++ func4_komar/func4_komar.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int age;
+	cout << "Введите свой возраст: ";
+	cin >> age;
+	age *= 12;
+	cout << "Ваш возраст в месяцах: " << age << endl;
+}
+
